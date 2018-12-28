a28447125b8068a0a94e18897918c6fbe4d3e587
Alban de Crevoisier <albandecrevoisier@gmail.com>
2018-12-28 15:00:46 +0100

    Add sklearn preprocessing pipelines.

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -1,24 +1,24 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+from sklearn.compose import ColumnTransformer
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
-data = pd.read_csv("train.csv", index_col='PassengerId')
+train = pd.read_csv("train.csv", index_col='PassengerId')
+y = train.pop('Survived').values
+X = train.drop(['Name', 'Ticket', 'Cabin'], axis=1)
 
-X = data.drop(['Survived', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'],
-    axis=1)
-for c in X:
-    X[c].fillna(X[c].mean(), inplace = True)
-y = data['Survived']
+is_num = np.array([dt is not np.dtype('O') for dt in train.dtypes])
+num_si_step = ('num_si', SimpleImputer())
+ss_step = ('ss', StandardScaler())
+num_pl = Pipeline([num_si_step, ss_step])
+cat_si_step = ('cat_si', SimpleImputer(strategy='most_frequent'))
+ohe_step = ('ohe', OneHotEncoder(sparse=False, handle_unknown='ignore'))
+cat_pl = Pipeline([cat_si_step, ohe_step])
+ct = ColumnTransformer([('num_pl', num_pl, is_num),
+                        ('cat_pl', cat_pl, ~is_num)])
 
-test = pd.read_csv("test.csv", index_col='PassengerId')
-test = test.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1)
-for c in test:
-    test[c].fillna(test[c].mean(), inplace = True)
-
-clf = RandomForestClassifier().fit(X, y)
-pred = clf.predict(test)
-p = pd.DataFrame(pred, columns=['Survived'])
-pd.concat([pd.DataFrame(test.index), p], axis=1).to_csv("sub.csv", index=False)
