2012646bbf19dbdd2fc3353d3ed88092e40d1ba1
Alban de Crevoisier <albandecrevoisier@gmail.com>
2018-12-29 23:05:15 +0100

    Refine preprocessing.

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder,StandardScaler
+from sklearn.preprocessing import OneHotEncoder,StandardScaler,MinMaxScaler
 from sklearn.compose import ColumnTransformer
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -12,13 +12,20 @@ train = pd.read_csv("train.csv", index_col='PassengerId')
 y = train.pop('Survived').values
 X = train.drop(['Name', 'Ticket', 'Cabin'], axis=1)
 
-is_num = np.array([dt is not np.dtype('O') for dt in train.dtypes])
-num_si_step = ('num_si', SimpleImputer())
-ss_step = ('ss', StandardScaler())
-num_pl = Pipeline([num_si_step, ss_step])
+cat = ['Pclass', 'Sex', 'Embarked']
 cat_si_step = ('cat_si', SimpleImputer(strategy='most_frequent'))
-ohe_step = ('ohe', OneHotEncoder(sparse=False, handle_unknown='ignore'))
-cat_pl = Pipeline([cat_si_step, ohe_step])
-ct = ColumnTransformer([('num_pl', num_pl, is_num),
-                        ('cat_pl', cat_pl, ~is_num)])
+cat_ohe_step = ('cat_ohe',
+                OneHotEncoder(sparse=False, handle_unknown='ignore'))
+cat_pipe = Pipeline([cat_si_step, cat_ohe_step])
+age_si_step = ('age_si', SimpleImputer())
+age_mm_step = ('age_mm', MinMaxScaler())
+age_pipe = Pipeline([age_si_step, age_mm_step])
+num_si_step = ('num_si', SimpleImputer())
+num_ss_step = ('num_ss', StandardScaler())
+num_pipe = Pipeline([num_si_step, num_ss_step])
+ct = ColumnTransformer([('cat_pipe', cat_pipe, cat),
+                        ('age_pipe', age_pipe, ['Age'])],
+                        remainder=num_pipe,
+                        n_jobs=-1)
+print(ct.fit_transform(X))
 
