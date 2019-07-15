32fe3f6937ec32d5d19911c80fde66502b5c3a2b
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-07-16 01:06:46 +0200

    Try CNN with keras.

--- digit-recognizer/drec.py
+++ digit-recognizer/drec.py
@@ -1,27 +1,45 @@
-#!/usr/bin/python
-""" Digit recognizer """
+""" Kaggle - Digit Recognizer - MNIST """
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import cross_val_score
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.optimizers import Adadelta
+from tensorflow.keras.utils import to_categorical
 
 
 dataset = pd.read_csv("train.csv")
 
-y = dataset.pop('label')
-X = dataset
+y = to_categorical(dataset.pop('label'))
+X = np.array(dataset).reshape(-1, 28, 28, 1) / 255
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
 
-clf = make_pipeline(StandardScaler(), MLPClassifier((20, 20, 20, 20)))
+model = Sequential()
+model.add(Conv2D(64, 3, padding='same', activation='relu',
+    input_shape=(28, 28, 1)))
+model.add(Conv2D(64, 3, activation='relu'))
+model.add(MaxPooling2D(2))
+model.add(Dropout(0.05))
 
-print(cross_val_score(clf, X_train, y_train, cv=5))
+model.add(Conv2D(128, 3, padding='same', activation='relu'))
+model.add(Conv2D(128, 3, activation='relu'))
+model.add(MaxPooling2D(2))
+model.add(Dropout(0.05))
 
-clf.fit(X_train, y_train)
-print(clf.score(X_test, y_test))
+model.add(Flatten())
+model.add(Dense(28, 'relu'))
+model.add(Dropout(0.1))
+model.add(Dense(10, 'softmax'))
 
-test = pd.read_csv("test.csv")
-p = pd.DataFrame({'ImageId': test.index + 1, 'Label': clf.predict(test)})
-p.to_csv("sub.csv", index=False)
+model.compile(loss='categorical_crossentropy',
+    optimizer=Adadelta(),
+    metrics=['accuracy'])
+
+model.fit(X_train, y_train, validation_data=(X_test, y_test))
+
+#test = pd.read_csv("test.csv")
+#p = pd.DataFrame({'ImageId': test.index + 1, 'Label': clf.predict(test)})
+#p.to_csv("sub.csv", index=False)
