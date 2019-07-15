b7103072e659234fe1248fdc6de47f364cc1af40
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-07-15 18:03:44 +0200

    Use new sklearn neural_network, as it's better suited to such exploratory tasks.

--- digit-recognizer/drec.py
+++ digit-recognizer/drec.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python
 """ Digit recognizer """
 import numpy as np
 import pandas as pd
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import cross_val_score
 import maptplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
-
-
