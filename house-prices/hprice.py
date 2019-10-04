721bd94a430ba8e76581c61fdbbcc488852090f8
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-10-04 13:53:22 +0200

    List numerical features.

--- house-prices/hprice.py
+++ house-prices/hprice.py
@@ -1,7 +1,33 @@
 import numpy as np
 import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
 train = pd.read_csv("train.csv")
+
+numerical_features = ['LotFrontage',
+                      'LotArea',
+                      'YearBuilt',
+                      'YearRemodAdd',
+                      'MasVnrArea',
+                      'BsmtFinSF1',
+                      'BsmtFinSF2',
+                      'BsmtUnfSF',
+                      'TotalBsmtSF',
+                      '1stFlrSF',
+                      '2ndFlrSF',
+                      'LowQualFinSF',
+                      'GrLivArea',
+                      'GarageYrBlt',
+                      'GarageArea',
+                      'WoodDeckSF',
+                      'OpenPorchSF',
+                      'EnclosedPorch',
+                      '3SsnPorch',
+                      'ScreenPorch',
+                      'PoolArea',
+                      'MiscVal']
