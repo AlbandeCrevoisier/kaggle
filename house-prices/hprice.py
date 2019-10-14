a670bcd28e93abfbb0fa177c4565cd13a2c3fbf9
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-10-14 17:12:19 +0200

    Clean up import.

--- house-prices/hprice.py
+++ house-prices/hprice.py
@@ -1,6 +1,6 @@
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import BayesianRidge
 from sklearn.ensemble import GradientBoostingRegressor
 
 train = pd.read_csv("train.csv")
@@ -30,12 +30,17 @@ numerical_features = ['LotFrontage',
 X = train[['SalePrice'] + numerical_features].dropna()
 y = X.pop('SalePrice').values
 
+br = BayesianRidge()
+print(cross_val_score(br, X, y, cv=10).mean())
+br.fit(X, y)
+
 gbrt = GradientBoostingRegressor()
+print(cross_val_score(gbrt, X, y, cv=10).mean())
 gbrt.fit(X, y)
 
 test = pd.read_csv("test.csv", index_col='Id')
 test = test[numerical_features].fillna(test.mean())
 
-p = pd.DataFrame({'Id': test.index,
-                  'SalePrice': gbrt.predict(test)})
-p.to_csv("sub.csv", index=False)
+p = pd.DataFrame({'SalePrice': br.predict(test)},
+                 test.index)
+p.to_csv("sub.csv")
