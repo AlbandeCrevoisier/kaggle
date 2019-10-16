83ee2baba9ced3ba9f3a2794c527d556f89d685e
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-10-16 13:35:00 +0200

    Select some features to try with bayesian ridge regression.

--- house-prices/hprice.py
+++ house-prices/hprice.py
@@ -1,46 +1,43 @@
 import pandas as pd
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import BayesianRidge
-from sklearn.ensemble import GradientBoostingRegressor
+
 
 train = pd.read_csv("train.csv")
 
-numerical_features = ['LotFrontage',
-                      'LotArea',
-                      'YearBuilt',
-                      'YearRemodAdd',
-                      'MasVnrArea',
-                      'BsmtFinSF1',
-                      'BsmtFinSF2',
-                      'BsmtUnfSF',
-                      'TotalBsmtSF',
-                      '1stFlrSF',
-                      '2ndFlrSF',
-                      'LowQualFinSF',
-                      'GrLivArea',
-                      'GarageYrBlt',
-                      'GarageArea',
-                      'WoodDeckSF',
-                      'OpenPorchSF',
-                      'EnclosedPorch',
-                      '3SsnPorch',
-                      'ScreenPorch',
-                      'PoolArea',
-                      'MiscVal']
-X = train[['SalePrice'] + numerical_features].dropna()
+numerical_features = [
+    'LotFrontage',
+    'LotArea',
+    'YearBuilt',
+    'YearRemodAdd',
+    'TotalBsmtSF',
+    '1stFlrSF',
+    '2ndFlrSF',
+    'GarageArea',
+    'OpenPorchSF',
+    'EnclosedPorch',
+    'PoolArea']
+categorical_features = [
+    'Street',
+    'OverallQual',
+    'OverallCond',
+    'CentralAir',
+    'SaleType',
+    'SaleCondition']
+
+X = train[['SalePrice'] + numerical_features + categorical_features].dropna()
 y = X.pop('SalePrice').values
 
+#TODO pipe to handle categorical_features
+
 br = BayesianRidge()
-print(cross_val_score(br, X, y, cv=10).mean())
+print(cross_val_score(br, X, y, cv=5).mean())
 br.fit(X, y)
 
-gbrt = GradientBoostingRegressor()
-print(cross_val_score(gbrt, X, y, cv=10).mean())
-gbrt.fit(X, y)
-
 test = pd.read_csv("test.csv", index_col='Id')
-test = test[numerical_features].fillna(test.mean())
+test = test[numerical_features + categorical_features].fillna(test.mean())
 
-p = pd.DataFrame({'SalePrice': br.predict(test)},
-                 test.index)
+p = pd.DataFrame(
+    {'SalePrice': br.predict(test)},
+    test.index)
 p.to_csv("sub.csv")
